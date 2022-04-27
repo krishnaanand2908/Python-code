90eb57584a95eeb0b3bd5d567c7765b337ccc2a5
krishnaanand2908 <krishnaanand290809@gmail.com>
2022-04-27 15:40:09 +0530

    Revision

---
+++ comments, escape sequences and print statement.py
@@ -0,0 +1,27 @@
+#signlie line comment
+"""multi line comment"""
+
+""" 
+multi
+line
+comment
+"""
+# You can also use ctrl + / for converting a line into a comment!
+# You can also use ctrl + / for converting a line into a comment!
+# You can also use ctrl + / for converting a line into a comment!
+# You can also use ctrl + / for converting a line into a comment!
+
+print("For merging two print statements, you can simpleily use the end function.", end = " ")
+print("Thus, I merged these two print statements")
+print("You can type anything in the end function.")
+
+print("This also", "Works!")
+
+print("C;\\Krishna") #\\ is used to print \
+print("\"Hello\"") #\" is used to print "
+
+#\n is a new line character
+#\t adds a tab  
+
+print("Hi\nBye")
+print("Hello\tBollo")
