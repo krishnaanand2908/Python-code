7644fb86cff74b0f8baa75086cf8a74a52f6b578
krishnaanand2908 <krishnaanand290809@gmail.com>
2022-04-27 15:22:06 +0530

    HelloWorld

--- Pro codes/krishna.py
+++ Pro codes/krishna.py
@@ -1,3 +1,12 @@
-def krish():
+def krish1():
     a = print("Krishna is a good boy!")
+    return a
+def krish2():
+    a = print("Krishna5 is a good boy!")
+    return a
+def krish3():
+    a = print("Kr3ishna is a good boy!")
+    return a
+def krish4():
+    a = print("Krishna 6s a good boy!")
     return a
